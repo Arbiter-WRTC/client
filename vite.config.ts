20df3114b2bbd2566e756429721fe266ad73c573
Sundar Jayabose <jayabose@gmail.com>
2023-12-09 14:31:07 -0500

    npm package
    used vite build.lib setting to build Arbiter component, https://vitejs.dev/guide/build.html\#library-mode
    
    Co-authored-by: Tyler Frye <tyler.frye42@gmail.com>
    Co-authored-by: Stephen Watzman <Ixisixi@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react(), basicSsl()], // legacy
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/components/Arbiter.tsx'),
+      name: 'Arbiter',
+      // the proper extensions will be added
+      fileName: 'Arbiter',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'react',
+        },
+      },
+    },
+  },  
 })
